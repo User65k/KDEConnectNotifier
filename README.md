2266e0a6c43583c7327c0aa3542395b5eeeddb5f
User65k <d4n@posteo.de>
2018-08-29 18:20:36 +0200

    pair mobiles

--- README.md
+++ README.md
@@ -3,4 +3,17 @@ Send notifications to KDEConnect and receive commands.
 
 I use it as a door bell notification with the [DoorBerryServer](https://github.com/User65k/DoorBerryServer)
 
-All cedit to <https://github.com/bboozzoo/kdeconnect-python-mock>.
+All cedit for KDE Connect in Python to <https://github.com/bboozzoo/kdeconnect-python-mock>.
+
+## Usage
+
+First pair all peers you want to talk to:
+```sh
+python3 pair_cli.py
+```
+
+Then run in the background:
+```sh
+python3 listener.py &
+```
+Trigger the notification by sending something to TCP/11000 and initialize a connection to TCP/14000 by using the KDEConnect Command Plugin. You probably what to change that in `listener.py`.
---
+++ pair_cli.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# AUTHORS
+# User65k
+#
+
+# send broadcast
+# -> get connected
+# -> recv ident
+# -> recv key
+# -> send key ?
+# listen to broadcast
+# -> connect
+# -> send ident
+# -> sent key
+# -> recv key
+
+import sys
+import socket
+import logging
+
+from Crypto.Cipher import PKCS1_v1_5
+from select import select
+
+from KDEConnectNotifier.consts import PAIRING, KEY_PEER, DISCOVERY_PORT
+from KDEConnectNotifier.kde_con_proto import get_key, handle_identity, send_identity, netpkt
+
+LISTENING_PORT = 1715
+
+def main():
+    pkey = get_key().publickey().exportKey()
+    
+    # listen for packet on UDP socket
+    discovery = socket.socket(type=socket.SOCK_DGRAM)
+    discovery.bind(('0.0.0.0', DISCOVERY_PORT))
+
+    #listen for new clients
+    server = socket.socket()
+    server.bind(('0.0.0.0', LISTENING_PORT))
+    server.listen(5)
+
+    wait_for = [discovery, server]
+    connections = {}
+
+    while True:
+        rl = select(wait_for,[],[], 1)[0]
+        for sckt in rl:
+            # send broadcast
+            # -> get connected
+            # -> recv ident
+            # listen to broadcast
+            # -> connect
+            # -> send ident
+            if sckt==discovery:
+                #a new client is waiting for us to connect
+                data, sender = discovery.recvfrom(1024)
+
+                devID, tcp_port = handle_identity(data, get_unpaired=True)
+
+                if devID:
+                    logging.debug('Device %s is at tcp://%s:%d', devID, sender[0], tcp_port)
+
+                    #init new connection
+                    ts = socket.socket()
+                    ts.connect((sender[0], tcp_port))
+                    send_identity(ts)
+
+                    connections[ts] = devID
+                    wait_for.append(ts)
+
+            elif sckt==server:
+                #a new client is connecting
+                ts, client_address = server.accept()
+
+                devID, p = handle_identity(ts.recv(4096), get_unpaired=True)
+                if devID:
+
+                    connections[ts] = devID
+                    wait_for.append(ts)
+                else:
+                    #no valid msg
+                    ts.close()
+
+        for con in wait_for:
+            if con==server or con==discovery:
+                continue
+            #ask user if its ok to pair
+            if cool:
+                send_pair(con, pkey)
+                # -> recv key
+                pkt = con.recv(4096).decode('ascii')
+                p = loads(pkt)
+                if p['type'] == PAIRING:
+                    with open(KEY_PEER % (connections[con]), 'w') as ref:
+                        ref.write(p['body']['publicKey'])
+            else:
+
+                pkt = netpkt(PAIRING, {'pair': False})
+                ts.send(pkt)
+                ts.send(b'\n')
+
+
+if __name__ == '__main__':
+    main()
