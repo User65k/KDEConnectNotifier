e5d956d5b748939d99db71cdd95d80f97f6bcdef
User65k <d4n@posteo.de>
2018-08-29 17:53:38 +0200

    get unpaired devIDs

--- KDEConnectNotifier/kde_con_proto.py
+++ KDEConnectNotifier/kde_con_proto.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# AUTHORS
+# Maciek Borzecki <maciek.borzecki (at] gmail.com> https://github.com/bboozzoo/kdeconnect-python-mock
+# User65k
+#
 
 import logging
 from json import loads, dumps
@@ -12,6 +32,12 @@ from KDEConnectNotifier.consts import PAIRING, NOTIFICATION, RUNCOMMAND, ENCRYPT
 
 
 def netpkt(tp, data):
+    """
+    Construct a new packet
+
+    :tp: type of data packet
+    :data: body of data packet
+    """
     d = {
         'id': int(time() * 1000),
         'type': tp,
@@ -21,7 +47,7 @@ def netpkt(tp, data):
 
 def send_identity(ts):
     """
-    :ts: socket
+    :param socket.socket ts: socket to send on
     """
     pkt = netpkt(IDENTITY,
                 {
@@ -38,7 +64,7 @@ def send_identity(ts):
 
 def send_pair(ts, key):
     """
-    :param socket.socket ts: socket
+    :param socket.socket ts: socket to send on
     :param: PEM encoded public key
     """
     # logging.debug('public key: %s', key)
@@ -52,6 +78,10 @@ def send_pair(ts, key):
 def handle_packets(pkts, cipher, host, socket, callbacks):
     """
     :param list pkts: list of packets
+    :param Crypto.Cipher.PKCS1_v1_5 cipher: decryption class
+    :param str host: DeviceID of the peer we talk to
+    :param socket.socket socket: socket
+    :param dict callbacks: TYPE -> function(body, host, socket)
     """
     for pkt in pkts:
         try:
@@ -100,6 +130,11 @@ def handle_packets(pkts, cipher, host, socket, callbacks):
             logging.exception(pkt, exc_info=e)
 
 def send_crypted(clear, host, socket):
+    """
+    Send encrypted packet to peer
+    :param str host: DeviceID of the peer we talk to
+    :param socket.socket socket: socket
+    """
     key = None
     with open(KEY_PEER % (host), 'rb') as inf:
         key = RSA.importKey(inf.read())
@@ -127,7 +162,15 @@ def get_key():
             outf.write(key.exportKey('PEM'))
     return key
 
-def handle_identity(data):
+def handle_identity(data, get_unpaired=False):
+    """
+    Check if data steam is connected to a device
+
+    :param bytes data: bytes containing identity packet of peer
+    :param bool get_unpaired: also return unpaired devices. Delaut no
+    :return: tuple of deviceID and TCP Port to connect it
+    :rtype: tuple
+    """
     pkt = loads(data.decode('ascii'))
     logging.debug('discovered: %r', pkt)
     if pkt['type'] == IDENTITY:
@@ -136,7 +179,7 @@ def handle_identity(data):
         if 'tcpPort' in pkt['body']:
             tcp_port = pkt['body']['tcpPort']
 
-        if path.exists(KEY_PEER % (devID)):
+        if get_unpaired or path.exists(KEY_PEER % (devID)):
             return devID, tcp_port
 
     return None, None
